0ca07e8b73da0337fa237849034400d5fdd66c97
JoseFlores25 <74205482+JoseFlores25@users.noreply.github.com>
2023-01-02 19:53:20 -0500

    Done

---
+++ app/controllers/birds_watcher_controller.rb
@@ -0,0 +1,12 @@
+class BirdsWatcherController < ApplicationController
+  attr_accessor :name, :email, :bio, :favorite_species, :admin
+
+  def initialize(args)
+    @name = args[:name]
+    @email = args[:email]
+    @bio = args[:bio]
+    @favorite_species = args[:favorite_species]
+    @admin = args[:admin]
+  end
+
+end
